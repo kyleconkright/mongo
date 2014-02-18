a734ddcc93b5b54ec707bf95b1868590821087a9
Kyle <kyleconkright@gmail.com>
2014-02-17 17:43:48 -0800

    Mongo is working
    Working on the Todo example app.

--- app.js
+++ app.js
@@ -1,44 +1,57 @@
-
 /**
  * Module dependencies.
  */
 
-var express = require('express');
-var routes = require('./routes');
-var mongoose = require('mongoose');
-var user = require('./routes/user');
-var http = require('http');
-var path = require('path');
+var express = require('express'),
+  routes = require('./routes'),
+  http = require('http'),
+  path = require('path'),
+  mongoose = require('mongoose');
 
 var app = express();
 
+mongoose.connect('mongodb://localhost/todo_development');
+var Schema = mongoose.Schema;
+var ObjectId = Schema.ObjectId;
+
+var Task = new Schema({
+  task: String
+});
+
+var Task = mongoose.model('Task', Task);
+
 // all environments
 app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
-if ('development' == app.get('env')) {
+if ('development' === app.get('env')) {
   app.use(express.errorHandler());
 }
 
-mongoose.connect('mongodb://localhost/todo_development', function(err){
-	if(!err) {
-		console.log('connected to MongoDB!!!!');
-	} else {
-		throw err;
-	}
+app.get('/', routes.index);
+
+app.get('/tasks', function(req, res){
+  Task.find({}, function (err, docs) {
+    res.render('tasks/index', {
+      title: 'Todos index view',
+      docs: docs
+    });
+  });
 });
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/tasks/new', function(req, res) {
+	res.render('tasks/new.jade', {
+		title: 'New Task'
+	});
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
